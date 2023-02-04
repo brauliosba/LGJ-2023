811e5e9e985d6b4af642b9de6b4a16e05da86262
Jose Ysique <joseysique@gmail.com>
2023-02-04 11:10:30 -0500

    tutorial manager

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public BattleSeriesManager battleSeriesManager;
     public LevelManager levelManager;
+    public TutorialManager tutorialManager;
     private void Awake()
     {
         instance = this;
